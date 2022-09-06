feaa366e1bd1cf383f2a5b1327d3e0010227cfae
tigranargit <73362927+tigranargit@users.noreply.github.com>
2022-09-06 10:51:28 +0100

    added home.erb, signin.erb and tested / and /signin routes

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require "sinatra/reloader"
+require_relative 'lib/database_connection'
+require_relative 'lib/space_repository'
+require_relative 'lib/user_repository'
+
+DatabaseConnection.connect
+
+class Application < Sinatra::Base
+      configure :development do
+        register Sinatra::Reloader
+        also_reload 'lib/space_repository'
+        also_reload 'lib/user_repository'
+end
+
+get '/' do
+      return erb(:home)
+end 
+
+get '/signin' do
+      return erb(:signin)
+end 
+
+end 
