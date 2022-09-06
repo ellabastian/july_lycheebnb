af525a88039b56743c46e94cb7ad7f22e15fcbd6
tigranargit <73362927+tigranargit@users.noreply.github.com>
2022-09-06 15:06:57 +0100

    finished signup route and validation

--- app.rb
+++ app.rb
@@ -13,15 +13,39 @@ class Application < Sinatra::Base
         also_reload 'lib/user_repository'
 end
 
+
 get '/' do
-      erb :sign_up
+      erb :signup
 end 
 
 post '/signup' do 
-      return erb(:spaces)      
+      if params[:name] == nil || params[:email] == nil || params[:password] == nil
+            status 400
+            return ''
+      end 
 
+      name = params[:name]
+      email = params[:email]
+      password = params[:password]
+
+      repo = UserRepository.new
+      all_users = repo.all 
+
+      all_users.each do |user| 
+            if user.email.include? email   
+                  # Want to flash a message before redirecting
+                  # flash[:notice] = "Email already registered. Please log in" 
+                  return erb(:login)
+                  end 
+            new_user = User.new
+            new_user.name = name
+            new_user.email = email 
+            new_user.password = password
+
+            repo.create(new_user)
+            return erb(:spaces)
+            end 
 end 
-
 get '/signin' do
       erb :login
 end 
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -29,4 +29,44 @@ context 'GET /signin' do
 
       end 
 end 
+
+context 'POST /signup' do
+      it "should check the email address doesn't already exist(new email address given)" do
+            response = post('/signup', name: 'newuser', email: 'newuser@newuser.com', password: 'password1')
+
+
+            expect(response.status).to eq(200)
+            expect(response.body).to include '<p> Below you will find a selection of spaces you can book.'
+
+            repo = UserRepository.new
+            all_users = repo.all
+            expect(all_users.last.name).to eq 'newuser'
+
+      end 
+
+      it "should check the email address doesn't already exist(existing email address given" do
+            response = post('/signup', name: 'newuser', email: 'test1@email.com', password: 'password1')
+
+
+            expect(response.status).to eq(200)
+            expect(response.body).to include 'Email already registered. Please log in'
+
+      end 
+end 
+
+
+context 'POST /signup' do
+      it "should add a valid user and return the main spaces HTML page" do
+            response = post('/signup', name: 'newuser', email: 'newuser@newuser.com', password: 'password1')
+
+
+            expect(response.status).to eq(200)
+            expect(response.body).to include '<p> Below you will find a selection of spaces you can book.'
+
+            repo = UserRepository.new
+            all_users = repo.all
+            expect(all_users.last.name).to eq 'newuser'
+
+      end 
+end 
 end
