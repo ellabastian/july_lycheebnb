459ea30e0a70f3e24ea65e6a1cece24bd7b07233
hakareem <harithakareem72@gmail.com>
2022-09-06 15:44:21 +0100

    signup page

--- app.rb
+++ app.rb
@@ -18,34 +18,34 @@ class Application < Sinatra::Base
             erb :sign_up
       end 
 
-post '/signup' do 
-      if params[:name] == nil || params[:email] == nil || params[:password] == nil
-            status 400
-            return ''
-      end 
-
-      name = params[:name]
-      email = params[:email]
-      password = params[:password]
-
-      repo = UserRepository.new
-      all_users = repo.all 
-
-      all_users.each do |user| 
-            if user.email.include? email   
-                  # Want to flash a message before redirecting
-                  # flash[:notice] = "Email already registered. Please log in" 
-                  return erb(:login)
-                  end 
-            new_user = User.new
-            new_user.name = name
-            new_user.email = email 
-            new_user.password = password
-
-            repo.create(new_user)
-            return erb(:spaces)
+      post '/signup' do 
+            if params[:name] == nil || params[:email] == nil || params[:password] == nil
+                  status 400
+                  return ''
             end 
-end 
+
+            name = params[:name]
+            email = params[:email]
+            password = params[:password]
+
+            repo = UserRepository.new
+            all_users = repo.all 
+
+            all_users.each do |user| 
+                  if user.email.include? email   
+                        # Want to flash a message before redirecting
+                        # flash[:notice] = "Email already registered. Please log in" 
+                        return erb(:login)
+                        end 
+                  new_user = User.new
+                  new_user.name = name
+                  new_user.email = email 
+                  new_user.password = password
+
+                  repo.create(new_user)
+            end 
+                  redirect '/spaces'
+      end 
 
       # login pages
       get '/login' do
@@ -79,9 +79,7 @@ end
 
             return erb :spaces
       end
-      erb :spaces
       
-end
       # add a new space
       get '/space/new' do
             erb :space_new
@@ -110,4 +108,4 @@ end
 
             redirect '/spaces'
       end
-end 
+      end 
