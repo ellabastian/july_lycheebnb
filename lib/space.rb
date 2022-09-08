73f902c5fc86569444d0e024eaf3f22867f5dbd0
hakareem <harithakareem72@gmail.com>
2022-09-08 09:29:28 +0100

    space array

--- lib/space.rb
+++ lib/space.rb
@@ -1,3 +1,8 @@
 class Space
-  attr_accessor :id, :name, :description, :price_per_night, :available_from, :available_to, :requested, :confirmed, :user_id
+  attr_accessor :id, :name, :description, :price_per_night, :available_from, :available_to, :requested, :confirmed, :user_id, :array
+
+  
+  def initialize
+    self.array = []
+  end
 end
